105525b4f2af96fdcf4a806248c6b56f96959ade
Osinachi Chukwujama <Osinachi.Chukwujama@gmail.com>
2022-03-21 21:12:41 +0000

    chore: remove unecessary utils

--- utils/get_table_length.lua
+++
@@ -1,9 +0,0 @@
-function getTableLength(tbl)
-    local length = 0
-    for n in pairs(tbl) do 
-      length = length + 1 
-    end
-    return length
-end
-
-return getTableLength
--- utils/uuid.lua
+++
@@ -1,5 +0,0 @@
-local socket = require("socket")  -- gettime() has higher precision than os.time()
-local uuid = require("libraries.uuid")
--- see also example at uuid.seed()
-uuid.randomseed(socket.gettime()*10000)
-return uuid
